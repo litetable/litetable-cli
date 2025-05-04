063a0a47078649ef0ebe94a157274908d566a556
Tony Rizzotto <tony@tonyrizzotto.com>
2025-05-04 17:33:46 -0400

    feat: add sidebar

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,11 +1,14 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import AppLayout from "./layout.jsx";
 
 function App() {
   return (
-    <div>
-      <h1 className={"text-3xl"}>Welcome to LiteTable!</h1>
-      <Button className={"cursor-pointer"}>Click me</Button>
-    </div>
+    <AppLayout>
+      <div>
+        <h1 className={"text-3xl"}>Welcome to LiteTable!</h1>
+        <Button className={"cursor-pointer"}>Click me</Button>
+      </div>
+    </AppLayout>
   );
 }
 

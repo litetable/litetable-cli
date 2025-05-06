7b0ad6f53ca4caca46add71b39df777b7d30ab96
Tony Rizzotto <tony@tonyrizzotto.com>
2025-05-05 21:45:48 -0400

    fix: handle contract similarly

--- client/src/components/query-input.jsx
+++ client/src/components/query-input.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import {transformToTableData, unwrapAndDecodeData} from '@/utils.js';
+import { unwrapAndDecodeData } from "@/utils.js";
 
 // const keywords = ["READ", "WRITE", "DELETE", "family", "key", "qualifier", "value", "latest"];
 // const operations = ["READ", "WRITE", "DELETE"];

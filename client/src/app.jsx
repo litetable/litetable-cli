e7db498d5e310f08e7c4c0f2d8647ae39b150ac6
Tony Rizzotto <tony@tonyrizzotto.com>
2025-05-06 23:01:37 -0400

    chore: cleanup

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -5,9 +5,8 @@ function App() {
   return (
     <AppLayout>
       <div>
-        <h1 className={"text-3xl pb-4"}>Welcome to LiteTable!</h1>
+        <h1 className={"text-3xl py-4"}>Welcome to LiteTable!</h1>
         <QueryBuilder />
-        {/*<QueryInput />*/}
       </div>
     </AppLayout>
   );
